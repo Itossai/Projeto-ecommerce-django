32f8ffcd7136dfe54e0e2f7980f806f18580b3d2
Ittossai <dantedeleon.ti@gmail.com>
2025-12-20 20:36:23 -0300

    adicionando detalhe de pedido e listar pedidos

--- pedido/views.py
+++ pedido/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.contrib import messages
 from django.views import View
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from produto.models import Variacao
 from .models import Pedido,ItemPedido
@@ -11,22 +11,23 @@ from utils import utils
 # Create your views here.
 
 
-class DispatchLoginRequired(View):
+class DispatchLoginRequiredMixin(View):
     def dispatch(self,*args,**kwargs):
         if not self.user.is_authenticated:
             return redirect('perfil:criar')
         return super().dispatch(*args,**kwargs)
 
-class Pagar(DispatchLoginRequired,DetailView):
+    def get_queryset(self,*args,**kwargs):
+        qs = super().get_queryset(*args,**kwargs)
+        qs = qs.filter(usuario=self.request.user)
+        return qs
+class Pagar(DispatchLoginRequiredMixin,DetailView):
     template_name = 'pedido/pagar.html'
     model = Pedido
     pk_url_kwarg = 'pk'
     context_object_name = 'pedido'
 
-    def get_queryset(self,*args,**kwargs):
-        qs = super().get_queryset(*args,**kwargs)
-        qs = qs.filter(usuario=self.request.user)
-        return qs
+    
 
 
 class SalvarPedido(View):
@@ -118,8 +119,15 @@ class SalvarPedido(View):
                 )
             )
 
-class Detalhe(View):
-    ...
+class Detalhe(DispatchLoginRequiredMixin,DetailView):
+    model = Pedido
+    context_object_name = 'pedido'
+    template_name = 'detalhe.html'
+    pk_url_kwarg = 'pk'
 
-class Lista(View):
-    ...
+class Lista(DispatchLoginRequiredMixin,ListView):
+    model = Pedido
+    context_object_name = 'pedidos'
+    template_name = 'lista.html'
+    paginate_by = 10
+    ordering = ['-id']
